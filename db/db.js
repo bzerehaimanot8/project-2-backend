a967c76f876ce9d9e86bbfa5c0d9c95a630b0180
Besrat Zerehaimanot <bzerehaimanot@gmail.com>
2020-07-31 18:31:34 -0400

    basic server is up, created repo's in front/backend folders respectively

---
+++ db/db.js
@@ -0,0 +1,14 @@
+// const mongoURI = 'mongodb://localhost:27017/' + 'Roster'
+// const db = mongoose.connection
+
+// mongoose.connect(
+// mongoURI,
+// {
+// useNewUrlParser: true,
+// useUnifiedTopology: true
+// }, () => {
+// console.log('Connected to the database');
+
+// }
+// )
+
