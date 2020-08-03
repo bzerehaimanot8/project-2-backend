3f23f2ecdca510a0e90c4489ed8c72ccde4c6284
Besrat Zerehaimanot <bzerehaimanot@gmail.com>
2020-08-03 14:12:42 -0400

    added some more functions

--- controllers/players.js
+++ controllers/players.js
@@ -1,14 +1,29 @@
 const Player = require('../models/players.js')
 const playerData = require('../data/players.js')
 
-//index handler
+//index route
 const indexPlayer = async (req, res) => {
   res.json(await Player.find({}))
 }
 
 //seed routes
 const seedPlayer = async (req, res) => {
-  res.json(await Player.create({}))
+  res.json(await Player.create({playerData}))
 }
 
-module.exports = {indexPlayer, seedPlayer}
+//create route
+const createPlayer = async (req, res) => {
+  res.json(await Player.create(req.body))
+}
+
+//update
+const updatePlayer = async (req, res) => {
+  res.json(await Player.findByIdAndUpdate(req.params.id, req.body))
+}
+
+//destroy
+const destroyPlayer = async (req, res) => {
+  res.json(await Player.findByIdAndDelete(req.params.id))
+}
+
+module.exports = {indexPlayer, seedPlayer, createPlayer, updatePlayer, destroyPlayer}
